6571128e4f160d98aca2c80373157c85b07fdb18
tadegx <tadej.vozlic@gmail.com>
2018-10-20 15:36:39 +0200

    added lists of suggestions

--- src/app/components/advices/advices.component.ts
+++ src/app/components/advices/advices.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advices.component.css']
 })
 export class AdvicesComponent implements OnInit {
+  me = [ "Consume enough Vitamins", "rest", "Wash your hands"];
+  fluffy = ["Give him probiotics", "Keep him in house"];
+  rating = ["neutral","neutral","neutral"];
+  pet_rating = ["neutral","neutral"];
 
   constructor() { }
 
   ngOnInit() {
   }
+change_rating(rating, index){
+  console.log(rating, index);
+  this.rating[index] = rating;
+}
+change_pet_rating(rating, index){
+  console.log(rating, index);
+  this.pet_rating[index] = rating;
+}
 
 }
